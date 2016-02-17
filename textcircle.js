95e8e5a6ec41ff95a4e2c0e2de23cfcbe8891f6c
Michael Wang <michael.icwang@gmail.com>
2016-02-17 17:20:10 +0800

    Add document if none is found.

--- textcircle.js
+++ textcircle.js
@@ -1,8 +1,12 @@
+this.Documents = new Mongo.Collection("documents");
+
 if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (!Documents.findOne()) {
+      Documents.insert({title: "New document"});
+    }
   });
 }
