11e1526442492270ebd8532f79f96624afa5b65b
Michael Wang <michael.icwang@gmail.com>
2016-02-21 13:08:00 +0800

    Add editor template.

--- textcircle.js
+++ textcircle.js
@@ -1,6 +1,18 @@
 this.Documents = new Mongo.Collection("documents");
 
+
 if (Meteor.isClient) {
+
+  Template.editor.helpers({
+    docid: function() {
+      var doc = Documents.findOne();
+      if (doc) {
+        return Documents.findOne()._id;
+      } else {
+        return undefined;
+      }
+    }
+  });
 }
 
 if (Meteor.isServer) {
