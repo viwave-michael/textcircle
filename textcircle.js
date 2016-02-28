a73a97cce9706f61d02bdd946c89b5f5564b819e
michael-wang <michael.icwang@gmail.com>
2016-02-28 22:19:49 +0800

    Add editing user data.
    Remove insecure package.
    Remove accounts-ui-unstyled package to resolve conflict.

--- textcircle.js
+++ textcircle.js
@@ -1,5 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
-
+EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
 
@@ -20,6 +20,7 @@ if (Meteor.isClient) {
         editor.on("change", function(editor, info) {
           var code = editor.getValue()
           $("#viewer_iframe").contents().find("html").html(code);
+          Meteor.call("addEditingUser");
         });
       }
     }
@@ -33,3 +34,38 @@ if (Meteor.isServer) {
     }
   });
 }
+
+Meteor.methods({
+  // To construct data:
+  // {
+  //   "_id" : "xxx",
+  //   "docid" : "xxx",
+  //   "users" : {
+  //     "userid..." : {"first-name":"xxx", ..., "lastEdit":"xxx"},
+  //     "userid..." : {"first-name":"xxx", ..., "lastEdit":"xxx"},
+  //     "userid..." : {"first-name":"xxx", ..., "lastEdit":"xxx"}
+  //   }
+  // }
+  addEditingUser: function() {
+    var doc, user, editingUsers;
+    doc = Documents.findOne();
+    if (!doc) {
+      return;
+    }
+    if (!this.userId) {
+      return;
+    }
+    user = Meteor.user().profile;
+
+    editingUsers = EditingUsers.findOne({docid:doc._id});
+    if (!editingUsers) {
+      editingUsers = {
+        docid: doc._id,
+        users: {}
+      };
+    }
+    user.lastEdit = new Date();
+    editingUsers.users[this.userId] = user
+    EditingUsers.upsert({_id: editingUsers._id}, editingUsers);
+  }
+});
